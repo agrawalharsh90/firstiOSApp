6fa73c0d5964ab235429abb64471e06aa347ee56
Harsh Agrawal <agrawalharsh90@gmail.com>
2021-08-19 17:48:36 +0530

    Initial Commit

---
+++ firstiOSAPP/firstiOSAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstiOSAPPApp.swift
+//  firstiOSAPP
+//
+//  Created by Harsh Agrawal on 19/08/21.
+//
+
+import SwiftUI
+
+@main
+struct firstiOSAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

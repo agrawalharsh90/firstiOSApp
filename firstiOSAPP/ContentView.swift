bd8fd7779293fd200e57404ac3a83f74b01e6cdd
Harsh Agrawal <agrawalharsh90@gmail.com>
2021-08-26 07:39:44 +0530

    text view changed

--- firstiOSAPP/ContentView.swift
+++ firstiOSAPP/ContentView.swift
@@ -15,15 +15,13 @@ struct ContentView: View {
             Image("agrawalharsh90")
                 .resizable().aspectRatio(contentMode:.fit)
             Spacer()
-
-            HStack(content: {
-                Spacer()
-                Text("Hello!")
-                Spacer()
-                Text("World!")
-                Spacer()
-
-            })
+            Text("Harsh Agrawal")
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .background(Color.green)
+                .border(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/, width: 10)
+                .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
             Spacer()
 
         })
